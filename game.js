fd8c0172c3850a078559dacffb11d571b160a8f0
Tobias Gärdebro <tubbeg@gmail.com>
2025-05-17 20:55:00 +0200

    Can now play animation on click event

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-import {Scene,Game} from "phaser"
+import {Scene,Game, GameObjects, ANIMATION_STOP} from "phaser"
 import {createConfig} from "./config.js"
 
 //simple is better
@@ -7,7 +7,32 @@ const h = "hello";
 
 console.log(h,h,h);
 ///*
-class Example extends Scene
+
+
+function ghost_jump(){
+
+}
+
+//WIP class
+class GhostSprite extends GameObjects.Sprite {
+
+    constructor (scene, x, y)
+    {
+        super(scene, x, y);
+        this.setTexture("ghost");
+        this.setPosition(x, y);
+        this.jump = false;
+    }
+
+    preUpdate (time, delta)
+    {
+        super.preUpdate(time, delta);
+
+    }
+
+}
+
+class PlayerScene extends Scene
 {
     preload ()
     {
@@ -18,8 +43,25 @@ class Example extends Scene
     create ()
     {
         this.anims.createFromAseprite("ghost");
+        //const ghost = new GhostSprite(this,400,300);
+        //this.sprite = this.add.existing(ghost);
+        
         this.sprite = this.add.sprite(400, 300, "ghost");
-        this.sprite.play({ key: "jump", repeat: -1 });
+        this.sprite.play({ key: "normal", repeat: -1 });
+        this.input.on("pointerdown", (e) =>
+        {
+            this.sprite
+                .play({ key: "jump", frameRate: 5 })
+                .once(Phaser.Animations.Events.ANIMATION_COMPLETE, () =>
+                {
+                    this.sprite.play({ key: "normal", repeat: -1 });
+                });
+        });
+        
+    }
+
+    update (t,dt){
+        //this.input.key()
     }
 }
 
@@ -28,5 +70,5 @@ function runGame (conf){
     const g = new Game(conf);
 }
 
-const conf = createConfig(Example)
+const conf = createConfig(PlayerScene)
 runGame(conf);
