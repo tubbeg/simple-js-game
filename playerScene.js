c180dc4adb3a694784505bb1e4906624058a3560
Tobias Gärdebro <tubbeg@gmail.com>
2025-05-19 18:21:24 +0200

    Can now play animation when ghost sprite interacts with tower.

--- playerScene.js
+++ playerScene.js
@@ -2,6 +2,20 @@
 import {Scene, GameObjects, ANIMATION_STOP} from "phaser"
 import { addGhostSprite } from "./playerSprite.js";
 
+function addCastle (objFactory, sprite, physicsWorld){
+    const castle = objFactory.image(400,400, "dark_castle").setScale(3,3);
+    castle.body.setAllowGravity(false);
+    castle.body.setImmovable();
+    objFactory.collider(sprite, castle);
+    physicsWorld.on('collide', (go1, go2, b1, b2) =>
+    {
+        console.log("ghost is dead :/");
+        sprite.play({ key: "dead", frameRate: 2 });
+        go2.setAlpha(0.5);
+    });
+    return castle;
+}
+
 class PlayerScene extends Scene
 {
     preload ()
@@ -12,19 +26,8 @@ class PlayerScene extends Scene
 
     create ()
     {
-        this.castle = this.physics.add.image(400,400, "dark_castle").setScale(3,3);
-        //this.castle.allowGravity = false;
-        //this.castle.setGravityY(0);
-        this.castle.body.setAllowGravity(false);
-        //this.castle.setInteractive();
         this.sprite = addGhostSprite(this.physics.add, this.anims, this.input);
-        this.physics.add.collider(this.sprite, this.castle);
-
-        this.physics.world.on('collide', (gameObject1, gameObject2, body1, body2) =>
-        {
-            //gameObject1.setAlpha(1.5);
-            gameObject2.setAlpha(0.5);
-        });
+        this.castle = addCastle(this.physics.add, this.sprite, this.physics.world);
     }
 
     update (t,dt){
